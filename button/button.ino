e793a78cf80d6c45ee58e37dd9e3d748190185e7
Pierre Marijon <pierre@marijon.fr>
2017-10-07 19:31:17 +0200

    Add led for status of client

--- button/button.ino
+++ button/button.ino
@@ -21,6 +21,12 @@ HTTPClient http_ring;
 //http server
 ESP8266WebServer server(80);
 
+// Led light when they aren't connect
+int pin_led_no_wifi = 16;
+
+// Led message send
+int pin_led_message_send = 5;
+
 void handle_set_threshold()
 {
   threshold = server.arg("value").toInt();
@@ -30,13 +36,20 @@ void handle_set_threshold()
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // LED setting
+  pinMode(pin_led_no_wifi, OUTPUT);
+  pinMode(pin_led_message_send, OUTPUT);
   
   WiFi.begin(ssid, password);
   
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
-    }
+    digitalWrite(pin_led_no_wifi, HIGH);
+  }
+  
+  digitalWrite(pin_led_no_wifi, LOW);
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println(WiFi.localIP());
@@ -65,14 +78,32 @@ void loop() {
     http_ok.GET(); 
   }
   
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    //WiFi.begin(ssid, password);
+    digitalWrite(pin_led_no_wifi, HIGH);
+  }
+  else
+  {
+    digitalWrite(pin_led_no_wifi, LOW);
+  }
+  
   val = analogRead(A0);
   int mean = noise_mean();
+  
+  if(digitalRead(pin_led_message_send) == HIGH)
+  {
+    digitalWrite(pin_led_message_send, LOW);
+  }
 
   Serial.println(val);
   if(val < mean - threshold)
   {
     Serial.println("WIN");
-    http_ring.GET();
+    if(http_ring.GET() == HTTP_CODE_OK)
+    {
+      digitalWrite(pin_led_message_send, HIGH);
+    }  
   }
   
   ring_noise[noise_token] = val;
